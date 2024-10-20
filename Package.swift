40f46b885a41bf7d5e8bdb214966bd37906b5626
Jochen Bernard <jochenbernard@gmail.com>
2024-10-20 16:49:57 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,24 +1,20 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftHaptics",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftHaptics",
-            targets: ["SwiftHaptics"]),
+            targets: ["SwiftHaptics"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftHaptics"),
+        .target(name: "SwiftHaptics"),
         .testTarget(
             name: "SwiftHapticsTests",
             dependencies: ["SwiftHaptics"]
-        ),
+        )
     ]
 )
