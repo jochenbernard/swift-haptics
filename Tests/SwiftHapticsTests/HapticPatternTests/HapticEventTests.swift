3c888e4d142da9ff81c86d6fea1639d90fc0f8b6
Jochen Bernard <jochenbernard@gmail.com>
2024-10-23 22:58:06 +0200

    Add haptic event tests

---
+++ Tests/SwiftHapticsTests/HapticPatternTests/HapticEventTests.swift
@@ -0,0 +1,70 @@
+import Testing
+import CoreHaptics
+
+@testable import SwiftHaptics
+
+struct HapticEventTests {
+    let hapticEvent = MockHapticEvent(
+        time: 2.0,
+        intensity: 3.0,
+        sharpness: 4.0,
+        attackTime: 5.0,
+        decayTime: 6.0,
+        releaseTime: 7.0,
+        sustained: true
+    )
+
+    @Test
+    func baseHapticEvents() {
+        let baseHapticEvents = hapticEvent.baseHapticEvents.map {
+            $0 as! MockHapticEvent
+        }
+        let expectedBaseHapticEvents = [
+            MockHapticEvent(
+                time: 2.0,
+                intensity: 3.0,
+                sharpness: 4.0,
+                attackTime: 5.0,
+                decayTime: 6.0,
+                releaseTime: 7.0,
+                sustained: true
+            )
+        ]
+
+        #expect(baseHapticEvents == expectedBaseHapticEvents)
+    }
+
+    @Test
+    func coreParameters() {
+        let coreParameters = hapticEvent.coreParameters
+        let expectedCoreParameters = [
+            CHHapticEventParameter(
+                parameterID: .hapticIntensity,
+                value: 3.0
+            ),
+            CHHapticEventParameter(
+                parameterID: .hapticSharpness,
+                value: 4.0
+            ),
+            CHHapticEventParameter(
+                parameterID: .attackTime,
+                value: 5.0
+            ),
+            CHHapticEventParameter(
+                parameterID: .decayTime,
+                value: 6.0
+            ),
+            CHHapticEventParameter(
+                parameterID: .releaseTime,
+                value: 7.0
+            ),
+            CHHapticEventParameter(
+                parameterID: .sustained,
+                value: 1.0
+            )
+        ]
+
+        #expect(coreParameters.map(\.parameterID) == expectedCoreParameters.map(\.parameterID))
+        #expect(coreParameters.map(\.value) == expectedCoreParameters.map(\.value))
+    }
+}
